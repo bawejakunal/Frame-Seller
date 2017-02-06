c0adb2a00f01953a7e091acd2c88edc6f9469681
Siddharth Shah <sas2387@columbia.edu>
2017-02-06 01:22:19 -0500

    Added html and javascript for products. Pending payment processing

--- frontend/js/getProducts.js
+++ frontend/js/getProducts.js
@@ -35,72 +35,103 @@
     }
 
     function fillProduct(data) {
-       JSONArray = data;
-       var numProducts = JSONArray.length;
-       console.log("Number of products=" + numProducts);
-       var fullRows = Math.floor(numProducts / 3);
-       var incompleteRows = Math.floor(numProducts % 3);
-       var index = -1;
-       var photoSection = document.getElementById('photoSection');
-       try {
-           photoSection.innerHTML += '<div class="container">';
-           for (var i = 0; i < fullRows; i++) {
-               photoSection.innerHTML += '<div class="row">';
-               for (var j = 0; j < 3; j++) {
-                   index++;
-                   console.log(index);
-                   $("#photoSection" ).append('<div class="col-sm-4">\
-                   <img src="' + JSONArray[index].url + '" class="img-thumbnail img-responsive" alt="Image not found">\
-                   <figcaption>' + JSONArray[index].description + '</figcaption>\
-                   <div class="alert alert-info">\
-                   <strong>' + JSONArray[index].price + '</strong> \
-                   </div>\
-                   <form action="http://e4fb795b.ngrok.io/stripe_demo/order/" method="POST">\
-                   \x3Cscript\
-                   src="https://checkout.stripe.com/checkout.js" class="stripe-button"\
-                   data-key="pk_test_HbZ2G2ELK1pvsY2iOIKStdKn"\
-                   data-amount="' + JSONArray[index].price + '"\
-                   data-name="Photo Gallery Buy"\
-                   data-description="' + JSONArray[index].description + '"\
-                   data-image="https://stripe.com/img/documentation/checkout/marketplace.png"\
-                   data-locale="auto"\
-                   data-zip-code="true">\x3C/script>\
-                   <input type="hidden" value="' + JSONArray[index].id + '" name="productid">\
-                   </form>\
-                   </div>');
-               }
-               photoSection.innerHTML += '</div>';
-           }
-           photoSection.innerHTML += '<div class="row">';
-           for (var k = 0; k < incompleteRows; k++) {
-               index++;
-               console.log(index);
-               $("#photoSection" ).append('<div class="col-sm-4">\
-               <img src="' + JSONArray[index].url + '" class="img-thumbnail img-responsive" alt="Image not found">\
-               <figcaption>' + JSONArray[index].description + '</figcaption>\
-               <div class="alert alert-info">\
-               <strong>' + JSONArray[index].price + '</strong> \
-               </div>\
-               <form action="http://e4fb795b.ngrok.io/stripe_demo/order/" method="POST">\
-               \x3Cscript\
-               src="https://checkout.stripe.com/checkout.js" class="stripe-button"\
-               data-key="pk_test_HbZ2G2ELK1pvsY2iOIKStdKn"\
-               data-amount="' + JSONArray[index].price + '"\
-               data-name="Photo Gallery Buy"\
-               data-description="' + JSONArray[index].description + '"\
-               data-image="https://stripe.com/img/documentation/checkout/marketplace.png"\
-               data-locale="auto"\
-               data-zip-code="true">\x3C/script>\
-               <input type="hidden" value="' + JSONArray[index].id + '" name="productid">\
-               </form>\
-               </div>');
-           }
-           photoSection.innerHTML += '</div>';
-       }
-       catch (err) {
-           console.log("ERROR in getProducts.js:" + err);
-       }
-   }
+     JSONArray = data;
+     var numProducts = JSONArray.length;
+     console.log("Number of products=" + numProducts);
+     var fullRows = Math.floor(numProducts / 3);
+     var incompleteRows = Math.floor(numProducts % 3);
+     var index = -1;
+     var photoSection = document.getElementById('photoSection');
+     try {
+         photoSection.innerHTML += '<div class="container">';
+         for (var i = 0; i < fullRows; i++) {
+             photoSection.innerHTML += '<div class="row">';
+             for (var j = 0; j < 3; j++) {
+                 index++;
+                 console.log(index);
+
+                 var stripeString = "<div class=\"product alert alert-info col-sm-4\">\
+                 <img src=\"" + JSONArray[index].url + "\" class=\"img-thumbnail img-responsive\" alt=\"Image not found\">\
+                 <div class=\"caption\">\
+                <p>"+ JSONArray[index].description + "</p>\
+                </div>\
+                 <script src=\"https://checkout.stripe.com/checkout.js\"></script>\
+                 <button align=\"center\" class=\"btn btn-success\" id=\"customButton\">Pay $ "+JSONArray[index].price+"</button>\
+                 <script>\
+                 var handler = StripeCheckout.configure({\
+                  key: 'pk_test_HbZ2G2ELK1pvsY2iOIKStdKn',\
+                  image: 'https://stripe.com/img/documentation/checkout/marketplace.png',\
+                  locale: 'auto',\
+                  token: function(token) {\
+                    // You can access the token ID with `token.id`.\
+                    // Get the token ID to your server-side code for use.\
+                }\
+            });\
+            document.getElementById('customButton').addEventListener('click', function(e) {\
+              // Open Checkout with further options:\
+              handler.open({\
+                name: 'Stripe.com',\
+                description: 'Buy Photo Gallery',\
+                zipCode: true,\
+                amount: "+JSONArray[index].price+"\
+            });\
+            e.preventDefault();\
+        });\
+        // Close Checkout on page navigation:\
+        window.addEventListener('popstate', function() {\
+          handler.close();\
+      });\
+      </script>";
+
+      $("#photoSection" ).append(stripeString);
+  }
+  photoSection.innerHTML += '</div>';
+}
+photoSection.innerHTML += '<div class="row">';
+for (var k = 0; k < incompleteRows; k++) {
+ index++;
+ console.log(index);
+ var stripeString = "<div class=\"product alert alert-info col-sm-4\">\
+                 <img src=\"" + JSONArray[index].url + "\" class=\"img-thumbnail img-responsive\" alt=\"Image not found\">\
+                 <div class=\"caption\">\
+                <p>"+ JSONArray[index].description + "</p>\
+                </div>\
+                 <script src=\"https://checkout.stripe.com/checkout.js\"></script>\
+                 <button align=\"center\" class=\"btn btn-success\" id=\"customButton\">Pay $ "+JSONArray[index].price+"</button>\
+                 <script>\
+                 var handler = StripeCheckout.configure({\
+                  key: 'pk_test_HbZ2G2ELK1pvsY2iOIKStdKn',\
+                  image: 'https://stripe.com/img/documentation/checkout/marketplace.png',\
+                  locale: 'auto',\
+                  token: function(token) {\
+                    // You can access the token ID with `token.id`.\
+                    // Get the token ID to your server-side code for use.\
+                }\
+            });\
+            document.getElementById('customButton').addEventListener('click', function(e) {\
+              // Open Checkout with further options:\
+              handler.open({\
+                name: 'Stripe.com',\
+                description: 'Buy Photo Gallery',\
+                zipCode: true,\
+                amount: "+JSONArray[index].price+"\
+            });\
+            e.preventDefault();\
+        });\
+        // Close Checkout on page navigation:\
+        window.addEventListener('popstate', function() {\
+          handler.close();\
+      });\
+      </script>";
+
+      $("#photoSection" ).append(stripeString);
+}
+photoSection.innerHTML += '</div>';
+}
+catch (err) {
+ console.log("ERROR in getProducts.js:" + err);
+}
+}
 }
 
 function getCookie(cname) {
