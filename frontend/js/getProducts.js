8df3cf335243dc76cec2dca78f04b07a3fe5750e
Siddharth Shah <sas2387@columbia.edu>
2017-02-07 19:55:27 -0500

    Implemented Checkout. Error in POST order response from backend.

--- frontend/js/getProducts.js
+++ frontend/js/getProducts.js
@@ -1,32 +1,66 @@
 /**
  * Created by akshay,siddharth on 1/28/2017.
  */
-var handler = StripeCheckout.configure({
-                    key: 'pk_test_HbZ2G2ELK1pvsY2iOIKStdKn',
-                    image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-                    locale: 'auto',
-                    token: function(token) {
-                        console.log(token);
-                        // You can access the token ID with `token.id`.\
-                        // Get the token ID to your server-side code for use.\
-                    }
-});
-/*window.addEventListener('popstate', function() {
-    handler.close();
-});*/
+var orderEndpoint = "http://localhost:8000/stripe_demo/order/";
+var productEndPoint = "http://localhost:8000/stripe_demo/product/";
 
-function processPayment(price, productID){
-    console.log(price+" "+productID);
-       handler.open({
-           name: 'Stripe.com',
-           description: 'Buy Photo Gallery',
-           zipCode: true,
-           amount: price
-       });
+window.addEventListener('popstate', function () {
+    handler.close();
+});
+
+function processPayment(price, productID) {
+    var handler = stripe_checkout(productID);
+    handler.open({
+        name: 'Stripe.com',
+        description: 'Buy Photo Gallery',
+        zipCode: true,
+        amount: price * 100
+    });
 }
+
+function stripe_checkout(product_id) {
+    var handler = StripeCheckout.configure({
+        key: 'pk_test_HbZ2G2ELK1pvsY2iOIKStdKn',
+        image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+        locale: 'auto',
+        token: function (token) {
+            console.log("Send to Sir Baweja");
+            console.log("Product" + product_id);
+            console.log("Token");
+            console.log(token);
+
+            var jwttoken = getCookie("jwttoken");
+
+            var promise = new Promise(function (success, failure) {
+                $.ajax({
+                    url: orderEndpoint,
+                    type: 'POST',
+                    data: {'user':1, 'product_id': product_id, 'token': token.id},
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader('Authorization', 'JWT ' + jwttoken);
+                    },
+                    success: function (data) {
+                        success(data);
+                    },
+                    fail: function (data) {
+                        failure(data.responseText);
+                    },
+                });
+            });
+            promise.then(function (data) {
+                console.log(data);
+                fillProduct(data);
+            }, function (data) {
+                console.log(data);
+            });
+        }
+    });
+    return handler;
+}
+
+
 function getProducts() {
     console.log("get products demo call");
-    var JSONURL = 'http://localhost:8000/stripe_demo/product/';
     var jwttoken = getCookie("jwttoken");
     console.log("jwt token: " + jwttoken);
     if (!jwttoken) {
@@ -34,7 +68,7 @@ function getProducts() {
     } else {
         var promise = new Promise(function (success, failure) {
             $.ajax({
-                url: JSONURL,
+                url: productEndPoint,
                 type: 'GET',
                 beforeSend: function (xhr) {
                     xhr.setRequestHeader('Authorization', 'JWT ' + jwttoken);
@@ -79,33 +113,33 @@ function fillProduct(data) {
                     <p>" + JSONArray[index].description + "</p>\
                     </div>\
                      <script src=\"https://checkout.stripe.com/checkout.js\"></script>\
-                     <button align=\"center\" class=\"btn btn-success\" id=\"customButton\" onclick=\"processPayment(" + JSONArray[index].price + ",1);\">Pay $ " + JSONArray[index].price + "</button>";
+                     <button align=\"center\" class=\"btn btn-success\" id=\"customButton\" onclick=\"processPayment(" + JSONArray[index].price + "," + JSONArray[index].id + ");\">Pay $ " + JSONArray[index].price + "</button>";
 
 
-                    //     document.getElementById('customButton').addEventListener('click', function(e) {\
-                    //       // Open Checkout with further options:\
-                    //       handler.open({\
-                    //         name: 'Stripe.com',\
-                    //         description: 'Buy Photo Gallery',\
-                    //         zipCode: true,\
-                    //         amount: " + JSONArray[index].price + "\
-                    //     });\
-                    //     e.preventDefault();\
-                    // });\
-                    // Close Checkout on page navigation:\
-                  //   window.addEventListener('popstate', function() {\
-                  //     handler.close();\
-                  // });\
-                  // </script>";
+                //     document.getElementById('customButton').addEventListener('click', function(e) {\
+                //       // Open Checkout with further options:\
+                //       handler.open({\
+                //         name: 'Stripe.com',\
+                //         description: 'Buy Photo Gallery',\
+                //         zipCode: true,\
+                //         amount: " + JSONArray[index].price + "\
+                //     });\
+                //     e.preventDefault();\
+                // });\
+                // Close Checkout on page navigation:\
+                //   window.addEventListener('popstate', function() {\
+                //     handler.close();\
+                // });\
+                // </script>";
 
                 $("#photoSection").append(stripeString);
             }
-                    photoSection.innerHTML += '</div>';
+            photoSection.innerHTML += '</div>';
         }
         /////////////////////////////////////////////////////
         // Incomplete rows (having less than 3 products//////
         /////////////////////////////////////////////////////
-        if(incompleteRows>0){
+        if (incompleteRows > 0) {
             photoSection.innerHTML += '<div class="row">';
         }
         for (var k = 0; k < incompleteRows; k++) {
@@ -115,10 +149,10 @@ function fillProduct(data) {
                                  <div class=\"caption\">\
                                 <p>" + JSONArray[index].description + "</p>\
                                 </div>\
-                                 <button align=\"center\" class=\"btn btn-success\" id=\"customButton\" onclick=\"processPayment(" + JSONArray[index].price + ",1);\">Pay $ " + JSONArray[index].price + "</button>";
+                                 <button align=\"center\" class=\"btn btn-success\" id=\"customButton\" onclick=\"processPayment(" + JSONArray[index].price + "," + JSONArray[index].id + ");\">Pay $ " + JSONArray[index].price + "</button>";
             $("#photoSection").append(stripeString);
         }
-                photoSection.innerHTML += '</div>';
+        photoSection.innerHTML += '</div>';
     }
     catch (err) {
         console.log("ERROR in getProducts.js:" + err);
