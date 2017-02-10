813dbcb5d3c2fed94ab3072b59373887861a4b95
Siddharth Shah <sas2387@columbia.edu>
2017-02-10 00:03:17 -0500

    Made all the frond end code changes and reformatted code

--- frontend/js/catalog.js
+++ frontend/js/catalog.js
@@ -8,8 +8,8 @@ window.addEventListener('popstate', function () {
 function processPayment(price, productID) {
     var handler = stripe_checkout(productID);
     handler.open({
-        name: 'Stripe.com',
-        description: 'Buy Photo Gallery',
+        name: 'Frame Seller',
+        description: 'Buy Photo Frame',
         zipCode: true,
         amount: price * 100
     });
@@ -21,10 +21,7 @@ function stripe_checkout(product_id) {
         image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
         locale: 'auto',
         token: function (token) {
-            console.log(token);
-
             var jwttoken = getCookie("jwttoken");
-
             var promise = new Promise(function (success, failure) {
                 var orderdetails = {'product': product_id, 'token': token.id};
                 $.ajax({
@@ -44,20 +41,13 @@ function stripe_checkout(product_id) {
                 });
             });
             promise.then(function (data) {
-                console.log(data);
-                //if(data["success"] == true) {
-                    $("#snackbar").text("Your order has been submitted for processing");
-                    $("#snackbar").attr("class","show");
-
-                    // After 3 seconds, remove the show class from DIV
-                    setTimeout(function(){ $("#snackbar").attr("class",""); }, 3000);
-                //}
+                if (data["success"] == true) {
+                    showSnackbar("Your order has been submitted for processing");
+                } else {
+                    showSnackbar("Couldn't process your order. Please try again");
+                }
             }, function (data) {
-                $("#snackbar").text("Order processing failed");
-                $("#snackbar").attr("class","show");
-
-                // After 3 seconds, remove the show class from DIV
-                setTimeout(function(){ $("#snackbar").attr("class","") }, 3000);
+                showSnackbar("Couldn't process your order. Please try again");
             });
         }
     });
@@ -65,41 +55,36 @@ function stripe_checkout(product_id) {
 }
 
 
-function getProducts() {
-    console.log("get products demo call");
-    var jwttoken = getCookie("jwttoken");
-    console.log("jwt token: " + jwttoken);
-    if (!jwttoken) {
-        window.location.href = "index.html";
-    } else {
-        var promise = new Promise(function (success, failure) {
-            $.ajax({
-                url: productEndPoint,
-                type: 'GET',
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader('Authorization', 'JWT ' + jwttoken);
-                },
-                success: function (data) {
-                    success(data);
-                },
-                fail: function (data) {
-                    failure(data.responseText);
-                },
-            });
+function getProducts(jwttoken) {
+    var promise = new Promise(function (success, failure) {
+        $.ajax({
+            url: productEndPoint,
+            type: 'GET',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'JWT ' + jwttoken);
+            },
+            success: function (data) {
+                success(data);
+            },
+            error: function (data) {
+                failure(data.responseText);
+            },
         });
-        promise.then(function (data) {
-            console.log(data);
+    });
+    promise.then(function (data) {
+        if (data.length != 0) {
             fillProduct(data);
-        }, function (data) {
-            console.log(data);
-        });
-    }
+        } else {
+            showSnackbar("There are no products available at this time. Please try again later.");
+        }
+    }, function (data) {
+        showSnackbar("Can't fetch products currently. Please try again later");
+    });
 }
 
 function fillProduct(data) {
     JSONArray = data;
     var numProducts = JSONArray.length;
-    console.log("Number of products=" + numProducts);
     var fullRows = Math.floor(numProducts / 3);
     var incompleteRows = Math.floor(numProducts % 3);
     var index = -1;
@@ -115,12 +100,12 @@ function fillProduct(data) {
                 index++;
                 var stripeString = "<div class=\"col-sm-4\">\
                     <div class='product alert alert-info '>\
-                     <img src=\"" + JSONArray[index].url + "\" class=\"img-thumbnail img-responsive\" alt=\"Image not found\">\
-                     <div class=\"caption\">\
-                    <p>" + JSONArray[index].description + "</p>\
-                    </div>\
-                     <script src=\"https://checkout.stripe.com/checkout.js\"></script>\
-                     <button align=\"center\" class=\"btn btn-success\" id=\"customButton\" onclick=\"processPayment(" + JSONArray[index].price + "," + JSONArray[index].id + ");\">Pay $ " + JSONArray[index].price + "</button></div>";
+                     <img src=\"" + JSONArray[index].url + "\" class=\"img-thumbnail img-responsive\" \
+                     alt=\"Image not found\"> <div class=\"caption\"> <p>" + JSONArray[index].description + "</p>\
+                    </div> <script src=\"https://checkout.stripe.com/checkout.js\"></script>\
+                     <button align=\"center\" class=\"btn btn-success\" id=\"customButton\"\
+                     onclick=\"processPayment(" + JSONArray[index].price + "," + JSONArray[index].id + ");\">Pay $ "
+                    + JSONArray[index].price + "</button></div>";
 
                 $("#photoSection").append(stripeString);
             }
@@ -136,11 +121,12 @@ function fillProduct(data) {
             index++;
             var stripeString = "<div class=\"col-sm-4\">\
                                 <div class='product alert alert-info'>\
-                                 <img src=\"" + JSONArray[index].url + "\" class=\"img-thumbnail img-responsive\" alt=\"Image not found\">\
-                                 <div class=\"caption\">\
-                                <p>" + JSONArray[index].description + "</p>\
-                                </div>\
-                                 <button align=\"center\" class=\"btn btn-success\" id=\"customButton\" onclick=\"processPayment(" + JSONArray[index].price + "," + JSONArray[index].id + ");\">Pay $ " + JSONArray[index].price + "</button></div>";
+                                 <img src=\"" + JSONArray[index].url + "\" class=\"img-thumbnail img-responsive\"\
+                                 alt=\"Image not found\"> <div class=\"caption\">\
+                                <p>" + JSONArray[index].description + "</p> </div> <button align=\"center\"\
+                                class=\"btn btn-success\" id=\"customButton\" onclick=\"processPayment("
+                                + JSONArray[index].price + "," + JSONArray[index].id + ");\">Pay $ "
+                                + JSONArray[index].price + "</button></div>";
             $("#photoSection").append(stripeString);
         }
         photoSection.innerHTML += '</div>';
@@ -148,20 +134,4 @@ function fillProduct(data) {
     catch (err) {
         console.log("ERROR in getProducts.js:" + err);
     }
-}
-
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
 }
