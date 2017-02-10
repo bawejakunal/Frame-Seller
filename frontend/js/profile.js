d1468efcfadc75e872c0d5a425f1638330e44a15
Siddharth Shah <sas2387@columbia.edu>
2017-02-09 23:46:11 -0500

    Updated profile page

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -2,7 +2,6 @@ var orderEndpoint = "http://localhost:8000/stripe_demo/order/";
 
 function fillCustomerName(jwttoken) {
     var token = jwttoken.split('.')[1];
-    console.log(token);
     var decodedToken = atob(token);
     var tokenjson = JSON.parse(decodedToken);
     console.log(tokenjson["first_name"] + " " + tokenjson["last_name"]);
@@ -10,7 +9,6 @@ function fillCustomerName(jwttoken) {
 }
 
 function getOrdersForUser(jwttoken) {
-    console.log("token: " + jwttoken);
     var promise = new Promise(function (success, failure) {
         $.ajax({
             url: orderEndpoint,
@@ -21,43 +19,38 @@ function getOrdersForUser(jwttoken) {
             success: function (data) {
                 success(data);
             },
-            fail: function (data) {
+            error: function (data) {
                 failure(data.responseText);
             }
         });
     });
     promise.then(function (data) {
-        console.log(data);
-        fillOrders(data);
+        if (data.length != 0) {
+            fillOrders(data);
+        } else {
+            showSnackbar("You have no orders as of now");
+        }
     }, function (data) {
-        console.log(data);
+        showSnackbar("Failed to fetch orders. Please try again later.");
     });
 }
 
-function fillOrders(data){
+function fillOrders(data) {
     var orderSection = document.getElementById('orders');
     var numOrders = data.length;
-    for(var i=0; i< numOrders; i++){
-        try{
-            var paymentinfo = "";
-            if(data[i].paymentstatus == "UNPAID"){
-                paymentinfo = '<span class="paymentinfo alert alert-warning"><strong>PAYMENT STATUS</strong> <br> '+ data[i].paymentstatus+'</span>';
-            } else if (data[i].paymentstatus == "PAID"){
-                paymentinfo = '<span class="paymentinfo alert alert-success"><strong>PAYMENT STATUS</strong> <br> '+ data[i].paymentstatus+'</span>';
-            } else{
-                paymentinfo = '<span class="paymentinfo alert alert-danger"><strong>PAYMENT STATUS</strong> <br> '+ data[i].paymentstatus+'</span>';
-            }
-
+    for (var i = 0; i < numOrders; i++) {
+        try {
+            var paymentinfo = getPaymentInfoTag(data[i].product.paymentstatus);
             orderSection.innerHTML +=
-             '<div class="well well-lg">\
-                  <div class="row">\
-                        <div class="col-sm-2">\
-                            <img class="img-thumbnail img-responsive" src="'+ data[i].product.url +'">\
+                '<div class="well well-lg">\
+                     <div class="row">\
+                           <div class="col-sm-2">\
+                               <img class="img-thumbnail img-responsive" src="' + data[i].product.url + '">\
                             <div class="caption"><p>' + data[i].product.description + '</p></div>\
                         </div>\
                         <div class="col-sm-5">\
-                            <strong>Date Placed on:&nbsp; </strong> '+ getDateFromString(data[i].orderdate)+'<br><br>\
-                            <strong>Price:</strong> $ '+ data[i].product.price+'\
+                            <strong>Date Placed on:&nbsp; </strong> ' + getDateFromString(data[i].orderdate) + '<br><br>\
+                            <strong>Price:</strong> $ ' + data[i].product.price + '\
                         </div>\
                         <div class="col-sm-5">\
                             ' + paymentinfo + '\
@@ -65,8 +58,18 @@ function fillOrders(data){
                 </div>\
              </div>';
         }
-        catch (err){
+        catch (err) {
             console.log(err);
         }
+    }
+}
+
+function getPaymentInfoTag(orderStatus) {
+    if (orderStatus == "UNPAID") {
+        return '<span class="paymentinfo alert alert-warning"><strong>PAYMENT STATUS</strong> <br> ' + orderStatus + '</span>';
+    } else if (orderStatus == "PAID") {
+        return '<span class="paymentinfo alert alert-success"><strong>PAYMENT STATUS</strong> <br> ' + orderStatus + '</span>';
+    } else {
+        return '<span class="paymentinfo alert alert-danger"><strong>PAYMENT STATUS</strong> <br> ' + orderStatus + '</span>';
     }
 }
