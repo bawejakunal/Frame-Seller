7e16be82f6a7b0743975d5c0f37c289c4ac48b9e
Kunal Baweja <bawejakunal15@gmail.com>
2017-02-09 20:44:58 -0500

    update db url

--- stripe_demo/settings.py
+++ stripe_demo/settings.py
@@ -76,34 +76,18 @@ WSGI_APPLICATION = 'stripe_demo.wsgi.application'
 
 
 """
-Database
-http://docs.aws.amazon.com/elasticbeanstalk/latest/dg/create-deploy-python-rds.html
+Databases
 """
-if 'RDS_HOSTNAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stripedemo',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'modernstripe.cvkcqiaoutkr.us-east-1.rds.amazonaws.com',
+        'PORT': '3306'
     }
-
-else:
-    """
-    https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-    """
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'stripedemo',
-            'USER': 'root',
-            'PASSWORD': '',
-            'HOST': '127.0.0.1',
-        }
-    }
+}
 
 
 # Password validation
