ad488c5edb93cb00451381edf133f2232426bf12
Siddharth Shah <sas2387@columbia.edu>
2017-02-10 00:33:48 -0500

    Changes in login

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -25,94 +25,93 @@ function login() {
                 error: function (data) {
                     failure(data.responseText);
                 }
-            });
-
-            promise.then(function (data) {
-                setTokenCookie(data["token"]);
-                window.location.href = "catalog.html"
-            }, function (data) {
-                showSnackbar(data);
-            });
-        });
-    }
-}
-
-function signUp() {
-    firstname = $("#sufirstname");
-    lastname = $("#sulastname");
-    email = $("#suemail");
-    password = $("#supassword");
-    repeatpassword = $("#surepeatpassword");
-
-    var bool1 = check(firstname);
-    var bool2 = check(lastname);
-    var bool3 = check(email);
-    var bool4 = check(password);
-    var bool5 = check(repeatpassword);
-
-    if (bool1 && bool2 && bool3 && bool4 && bool5) {
-        parameters = {
-            'first_name': firstname.val(),
-            'last_name': lastname.val(),
-            'email': email.val(),
-            'password': password.val()
-        };
-        var promise = new Promise(function (success, failure) {
-            $.ajax({
-                url: signUpEndpoint,
-                type: 'POST',
-                contentType: "application/json",
-                data: JSON.stringify(parameters),
-                success: function (data) {
-                    success(data);
-                },
-                error: function (data) {
-                    failure(data.responseText);
-                }
-            });
+            })
         });
 
         promise.then(function (data) {
-            if (data["success"] == true) {
-                showSnackbar("User registered successfully. You can now login");
-            }
-            clearSignUpForm();
+            setTokenCookie(data["token"]);
+            window.location.href = "catalog.html"
         }, function (data) {
-            var json = JSON.parse(data);
-            if (Object.keys(json["error"]).length == 1) {
-                var errorKey = Object.keys(json["error"])[0];
-                showSnackbar(json["error"][errorKey]);
-            } else {
-                showSnackbar("An error occured with your sign up. Please check your credentials.");
-            }
+            showSnackbar(data["non_field_errors"]);
         });
     }
-}
 
-function clearSignUpForm() {
-    $("#sufirstname").val("");
-    $("#sulastname").val("");
-    $("#suemail").val("");
-    $("#supassword").val("");
-    $("#surepeatpassword").val("");
-}
+    function signUp() {
+        firstname = $("#sufirstname");
+        lastname = $("#sulastname");
+        email = $("#suemail");
+        password = $("#supassword");
+        repeatpassword = $("#surepeatpassword");
 
-function check(element) {
-    var bool;
-    if (element.val() == "") {
-        element.addClass("invalid");
-        bool = false;
-    } else {
-        element.removeClass("invalid");
-        bool = true;
-    }
+        var bool1 = check(firstname);
+        var bool2 = check(lastname);
+        var bool3 = check(email);
+        var bool4 = check(password);
+        var bool5 = check(repeatpassword);
 
-    if (element.attr('id') == "surepeatpassword" && element.val() !== "") {
-        if ($("#supassword").val() !== element.val()) {
-            alert("Passwords do not match");
-            element.addClass("invalid");
-            $("supassword").addClass("invalid");
+        if (bool1 && bool2 && bool3 && bool4 && bool5) {
+            parameters = {
+                'first_name': firstname.val(),
+                'last_name': lastname.val(),
+                'email': email.val(),
+                'password': password.val()
+            };
+            var promise = new Promise(function (success, failure) {
+                $.ajax({
+                    url: signUpEndpoint,
+                    type: 'POST',
+                    contentType: "application/json",
+                    data: JSON.stringify(parameters),
+                    success: function (data) {
+                        success(data);
+                    },
+                    error: function (data) {
+                        failure(data.responseText);
+                    }
+                });
+            });
+
+            promise.then(function (data) {
+                if (data["success"] == true) {
+                    showSnackbar("User registered successfully. You can now login");
+                }
+                clearSignUpForm();
+            }, function (data) {
+                var json = JSON.parse(data);
+                if (Object.keys(json["error"]).length == 1) {
+                    var errorKey = Object.keys(json["error"])[0];
+                    showSnackbar(json["error"][errorKey]);
+                } else {
+                    showSnackbar("An error occured with your sign up. Please check your credentials.");
+                }
+            });
         }
     }
-    return bool
-}
+
+    function clearSignUpForm() {
+        $("#sufirstname").val("");
+        $("#sulastname").val("");
+        $("#suemail").val("");
+        $("#supassword").val("");
+        $("#surepeatpassword").val("");
+    }
+
+    function check(element) {
+        var bool;
+        if (element.val() == "") {
+            element.addClass("invalid");
+            bool = false;
+        } else {
+            element.removeClass("invalid");
+            bool = true;
+        }
+
+        if (element.attr('id') == "surepeatpassword" && element.val() !== "") {
+            if ($("#supassword").val() !== element.val()) {
+                alert("Passwords do not match");
+                element.addClass("invalid");
+                $("supassword").addClass("invalid");
+            }
+        }
+        return bool
+    }
