e9d592d70ca92df3894e69f03b1b635ec36fe9f3
Kunal Baweja <bawejakunal15@gmail.com>
2017-03-16 18:27:25 -0400

    jwt token created

--- lambda/jwtoken.py
+++ lambda/jwtoken.py
@@ -2,24 +2,31 @@
 module to generate jwt token
 """
 
-import time
+import os
+from calendar import timegm
 import datetime
+from error import error
 from lib import jwt
 from lib.jwt.exceptions import MissingRequiredClaimError
 from lib.jwt import InvalidTokenError, DecodeError
 
-ALGORITHM = 'HS256'
-SECRET = 'SECRET'
-TIME_DELTA = 3600 #seconds
+try:
+    ALGORITHM = os.environ['JWT_ALGORITHM']
+    TIME_DELTA = int(os.environ['JWT_TIME_DELTA'])
+    SECRET = os.environ['JWT_SECRET']
+except KeyError:
+    error(500, 'Error loading JWT environment')
 
 def create_jwt(payload):
     """
     generate jwt token
     """
     exp = datetime.datetime.now() + datetime.timedelta(seconds=TIME_DELTA)
-    exp = int(time.mktime(exp.timetuple())) #Unix timestamp is always int
+    exp = timegm(exp.timetuple()) #Unix timestamp is always int
 
     payload['exp'] = exp
     token = jwt.encode(payload, SECRET, algorithm=ALGORITHM)
 
     return {'token':token}
+
+def verify_jwt(body)
