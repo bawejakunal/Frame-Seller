62259d9bdbbcdee6beb1a8db157453e49799c563
Akshay Nagpal <akshay2626@gmail.com>
2017-03-19 18:28:06 -0400

    email verification error handling

--- lambda/signup.py
+++ lambda/signup.py
@@ -1,6 +1,7 @@
 """
 User module to signup user
 """
+import os
 
 import boto3
 from botocore.exceptions import ClientError
@@ -37,54 +38,6 @@ def create_customer(body):
             },
             ConditionExpression="attribute_not_exists(uid) AND attribute_not_exists(email)"
         )
-        # email config BEGIN
-        email_client = boto3.client('ses')
-
-        email_response = email_client.send_email(
-            Source='akshay2626@gmail.com',
-            Destination={
-                'ToAddresses': [
-                    body['email'].strip(),
-                ],
-                'BccAddresses': [
-                ],
-                'CcAddresses': [
-                ],
-            },
-            Message={
-                'Subject': {
-                    'Data': 'Frameseller Email Verification',
-                    'Charset': 'UTF-8'
-                },
-                'Body': {
-                    'Text': {
-                        'Data': 'https://xyz.com/?token=hbsdhvjcbsdj&email='+body['email'].strip(),
-                        'Charset': 'UTF-8'
-                    },
-                    'Html': {
-                        'Data': '<html><a href="https://xyz.com/?token=hbsdhvjcbsdj&email='+body['email'].strip()+'"/></html>',
-                        'Charset': 'UTF-8'
-                    }
-                }
-            },
-            ReplyToAddresses=[
-                'akshay2626@gmail.com',
-            ],
-            ReturnPath='akshay2626@gmail.com',
-            SourceArn='string',
-            ReturnPathArn='string',
-            Tags=[
-                {
-                    'Name': 'signup_email',
-                    'Value': 'some_value'
-                },
-            ],
-            ConfigurationSetName='conf_set'
-        )
-
-        print email_response
-
-        # email config END
 
     except ClientError as err:
         if err.response['Error']['Code'] == 'ConditionalCheckFailedException':
@@ -94,6 +47,51 @@ def create_customer(body):
         return error(500, 'Error creating user entry')
 
     else:
+        # email config BEGIN
+        try:
+            email_client = boto3.client('ses',
+                                        aws_access_key_id=os.environ['LOCAL_AWS_ACCESS_KEY'],
+                                        aws_secret_access_key=os.environ['LOCAL_AWS_SECRET_KEY'],)
+            verification_token = os.urandom(16).encode('hex');
+            s3url = 'https://xyz.com/'
+            verification_url = s3url+'?vtoken='+verification_token+'&uemail='+body['email'].strip()
+
+            email_response = email_client.send_email(
+                Source='akshay2626@gmail.com',
+                Destination={
+                    'ToAddresses': [
+                        body['email'].strip(),
+                    ],
+                    'BccAddresses': [
+                    ],
+                    'CcAddresses': [
+                    ],
+                },
+                Message={
+                    'Subject': {
+                        'Data': 'Frameseller Email Verification',
+                        'Charset': 'UTF-8'
+                    },
+                    'Body': {
+                        'Text': {
+                            'Data': 'Use this url to verify:'+verification_url,
+                            'Charset': 'UTF-8'
+                        },
+                        'Html': {
+                            'Data': 'Use <a href="'+verification_url+'">this</a> url to verify.',
+                            'Charset': 'UTF-8'
+                        }
+                    }
+                },
+                ReplyToAddresses=[
+                    'akshay2626@gmail.com',
+                ],
+            )
+        except ClientError as email_err:
+            print email_err.response
+            return error(500, 'Error sending email')
+        # email config END
+
         return {
             'success': True,
             'message': 'Customer signup successful'
