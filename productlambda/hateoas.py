72f7d2a52d0e2539a3a9097f690c1065a32865aa
Akshay Nagpal <akshay2626@gmail.com>
2017-03-21 20:51:47 -0400

    fixed url path

--- productlambda/hateoas.py
+++ productlambda/hateoas.py
@@ -1,9 +1,12 @@
 def hateoas_constraints(mul_order, host, stage, path, productid=None):
     if productid:
+        host = host.strip().strip('/')
+        stage = stage.strip().strip('/')
+        path = path.strip().strip('/')
         if mul_order:
-            links = [{"rel": "self", "href": "https://" + host + "/" + stage + path + "/" + str(productid)}]
+            links = [{"rel": "self", "href": "https://" + host + "/" + stage + "/" + path + "/" + str(productid)}]
         else:
-            links = [{"rel": "self", "href": "https://" + host + "/" + stage + path}]
+            links = [{"rel": "self", "href": "https://" + host + "/" + stage + "/" + path}]
     else:
-        links = [{"rel": "products.list", "href": "https://" + host + "/" + stage + path}]
+        links = [{"rel": "products.list", "href": "https://" + host + "/" + stage + "/" + path}]
     return links
