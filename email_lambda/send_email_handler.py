a862ec05a13cd0111d1e5d45deb54107bdc298d1
Siddharth Shah <sas2387@columbia.edu>
2017-03-30 22:48:15 -0400

    Updated send email adapter

--- email_lambda/send_email_handler.py
+++ email_lambda/send_email_handler.py
@@ -16,22 +16,26 @@ def send_email_handler(event, context):
         workerName='sendemaillambdaworker'
     )
     
+    if response is None:
+        return
+    
+    print("taskToken")
+    print(urllib.quote_plus(response["taskToken"],""))
+    
+    taskToken = response["taskToken"]
+    input_json = json.loads(response["input"])
+    
+    if taskToken == "":
+        return
+    
     print(response["taskToken"])
     print(response["input"])
     print(type(response["input"]))
     
-    taskToken = urllib.quote_plus(response["taskToken"],"")
-    input_json = json.loads(response["input"])
-    
-    if input_json is None:
-        print "None"
-    else:
-        print input_json
-    
     """
     send verification email
     """
-    if 'uemail' not in input_json or 'vtoken' not in input_json or 'verify_page' not in input_json:
+    if 'jwt' not in input_json or 'verify_page' not in input_json or "email" not in input_json :
         print('400:Bad Request')
     try:
         aws_access_key_id = os.environ['LOCAL_AWS_ACCESS_KEY']
@@ -41,14 +45,18 @@ def send_email_handler(event, context):
                                    )
         s3url = input_json['verify_page'].strip().strip('/')
         
-        succeed_verification_url = (s3url + '?vtoken=' + input_json['vtoken'] + '&uemail='+input_json['uemail'] +'&taskToken='+taskToken + '&verify=succeed')
-        fail_verification_url = (s3url + '?vtoken=' + input_json['vtoken'] + '&uemail='+input_json['uemail'] +'&taskToken='+taskToken + '&verify=succeed')
+        params = {
+            "jwt" : input_json['jwt'],
+            "taskToken" : taskToken
+        }
+        
+        succeed_verification_url = (s3url + '?' + urllib.urlencode(params) )
                            
         response = email_client.send_email(
             Source='akshay2626@gmail.com',
             Destination={
                 'ToAddresses': [
-                    input_json['uemail'],
+                    input_json['email'],
                 ],
                 'BccAddresses': [
                 ],
@@ -62,11 +70,11 @@ def send_email_handler(event, context):
                 },
                 'Body': {
                     'Text': {
-                        'Data': 'Use this url to verify:'+succeed_verification_url+'. If you didn\'t register,click url:'+fail_verification_url,
+                        'Data': 'Use this url to verify:'+succeed_verification_url,
                         'Charset': 'UTF-8'
                     },
                     'Html': {
-                        'Data': 'Use <a href="'+succeed_verification_url+'">this url </a>to verify.If you didn\'t register,click on this <a href="'+fail_verification_url+'">url</a>',
+                        'Data': 'Use <a href="'+succeed_verification_url+'">this url </a>to verify.',
                         'Charset': 'UTF-8'
                     }
                 }
