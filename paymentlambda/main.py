6128e21e8a8309e53cf61fce0c2dbf41566d52e6
Kunal Baweja <bawejakunal15@gmail.com>
2017-04-21 00:32:40 -0400

    payment sqs

--- paymentlambda/main.py
+++ paymentlambda/main.py
@@ -2,23 +2,58 @@
 handle payment processing
 """
 import json
+import boto3
 from payment import create_charge
 from notify import publish, Topic
-from subscribe import Subscription
+from subscribe import Queue
 
 def handler(event, context):
     """
-    payment handler
+    payment handler invoked by Cloudwatch event
+    PaymentSchedule
     """
+    client = boto3.client('sqs')
+
+    response = client.receive_message(
+        QueueUrl=Queue.PAYMENT_URL,
+        AttributeNames=['All'],
+        WaitTimeSeconds=Queue.WAIT_TIME_S,
+        MaxNumberOfMessages=Queue.MAX_MESSAGES)
+
+    # ignore if no messages returned by queue
+    if 'Messages' not in response:
+        return
+
+    # process messages
+    messages = response['Messages']
+    processed_messages = list()
+
+    for message in messages:
+        payload = json.loads(message['Body']) #sqs message body
+        payment_request = json.loads(payload['Message']) #sns embedded message
+        charge_result = create_charge(payment_request)
+        publish(charge_result, Topic.PAYMENT)
+
+        _processed = {
+            'Id': message['MessageId'],
+            'ReceiptHandle': message['ReceiptHandle']
+        }
+        processed_messages.append(_processed)
+
+    #batch delete from queue
+    del_response = client.delete_message_batch(
+        QueueUrl=Queue.PAYMENT_URL,
+        Entries=processed_messages)
+
 
     #process SNS invoke
     #ignore non sns invokes
-    if 'Records' in event:
-        sns = event['Records'][0]['Sns']
-        topic_arn = sns['TopicArn']
+    # if 'Records' in event:
+    #     sns = event['Records'][0]['Sns']
+    #     topic_arn = sns['TopicArn']
 
-        #new order arrives, create payment else ignore
-        if Subscription[topic_arn] == 'order':
-            payload = json.loads(sns['Message'])
-            charge_result = create_charge(payload)
-            publish(charge_result, Topic.PAYMENT)
+    #     #new order arrives, create payment else ignore
+    #     if Subscription[topic_arn] == 'order':
+    #         payload = json.loads(sns['Message'])
+    #         charge_result = create_charge(payload)
+    #         publish(charge_result, Topic.PAYMENT)
