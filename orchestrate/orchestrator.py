6ae05b2f99ca8d3519e9eadc56ff5e6e516e79d7
Kunal Baweja <bawejakunal15@gmail.com>
2017-04-21 21:53:14 -0400

    TODO: 202 Location Header

--- orchestrate/orchestrator.py
+++ orchestrate/orchestrator.py
@@ -4,9 +4,8 @@ Lamdba Orchestrator
 
 from __future__ import print_function
 import json
-import orders
-import purchase
-from respond import respond, error
+from order import accept, validate
+from error import error
 from subscribe import Subscription
 from notify import Topic, publish
 
@@ -14,11 +13,10 @@ def handler(event, context):
     """
     delegate work
     """
-    print(event)
     try:
+        print(event)
         #process SNS messages here
         if 'Records' in event:
-
             sns = event['Records'][0]['Sns']
             topic_arn = sns['TopicArn']
 
@@ -26,33 +24,18 @@ def handler(event, context):
             if Subscription[topic_arn] == 'payment':
                 payload = json.loads(sns['Message'])
                 response = publish(payload, Topic.ORDER_UPDATE)
-
             else:
                 print('Subscribed topic %s not implemented' % topic_arn)
 
-        #handle API gateway invokes below
-        elif event['resource'].startswith('/orders'):
-            try:
-                response = orders.order(event)
-                data = json.loads(response["Payload"].read())
-                body = json.loads(data['body'])
-                return respond(data['statusCode'], body)
-            except (KeyError, Exception) as err:
-                #in case of unhandled exception
-                print(err)
-                return error(500, 'Error processing order request')
+        elif event['operation'] == 'purchase':
+            order_data = event['body-json']
+            if validate(order_data) is False:
+                return error(400, "Malformed data")
 
-        elif event['resource'].startswith('/purchase'):
-            status = None
-
-            #create order and publish to topic
-            data = purchase.buy_product(event)
-            body = json.loads(data['body'])
-
-            #return accepted with order url once
-            #payment/order accepted for processing
-            status = data['statusCode']
-            return respond(status, body)
+            # add to sqs and intermediate database
+            # extract order context from event
+            response = accept(event, order_data)
+            return error(202, response) #map this to 201 accepted response
 
         else:
             return error(500, "Unknown operation")
---
+++ orchestrate/order.py
@@ -0,0 +1,120 @@
+"""
+Call orders lambda service
+"""
+from datetime import datetime
+import json
+import boto3
+from calendar import timegm
+
+class Queue:
+    URL = 'https://sqs.us-east-1.amazonaws.com/908762746590/Order-Queue'
+
+def validate(order):
+    """
+    validate order json
+    """
+    _order_fields = ('product', 'stripe_token')
+    _product_fields = ('url', 'price', 'id', 'links',
+        'description')
+
+    if not isinstance(order, dict):
+        return False
+
+    for field in _order_fields:
+        if field not in order:
+            return False
+        if field == _order_fields[0] and not isinstance(order[field], dict):
+            return False
+        if field == _order_fields[1] and not isinstance(order[field], unicode):
+            return False
+
+    product = order['product']
+    for field in _product_fields:
+        if field not in product:
+            print order[field], type(order[field])
+            return False
+        if ((field == _product_fields[0] or field == _product_fields[4]) and
+                not isinstance(product[field], unicode)):
+            return False
+        if ((field == _product_fields[1] or field == _product_fields[2]) and
+                not isinstance(product[field], int)):
+            return False
+        if (field == _product_fields[3] and
+                not isinstance(product[field], list)):
+            return False
+
+    return True
+
+def accept(event, order):
+    """
+    accept valid order json
+    add to SQS and intermediate queue database
+    """
+    client = boto3.client('sqs')
+
+    #create message to send
+    _order_json = dict()
+    _order_json['type'] = 'create_order'
+    _order_json['data'] = order.copy()
+    _order_json['event'] = {
+        'principal-id': event['context']['authorizer-principal-id'],
+        'proto': event['params']['header']['CloudFront-Forwarded-Proto'],
+        'stage': event['context']['stage'],
+        'host': event['params']['header']['Host']
+    }
+
+    message = json.dumps(_order_json)
+
+    response = client.send_message(
+        QueueUrl=Queue.URL,
+        MessageBody=message)
+
+    return {'Location': 'http://queue.com'}
+
+# def invoke_order_lambda(payload, invoke='RequestResponse'):
+#     """
+#     invoke order lambda
+#     """
+#     response = boto3.client('lambda').invoke(
+#         FunctionName='orders',
+#         InvocationType=invoke,
+#         LogType='None',
+#         Payload=json.dumps(payload))
+
+#     return response
+
+# def order(event):
+#     """
+#     extract info from event and pass on to order microservice
+#     """
+#     payload = {
+#         'uid': event['requestContext']['authorizer']['principalId'],
+#         'host': event['headers']['Host'],
+#         'stage': event['requestContext']['stage'],
+#         'path': event['path'],
+#         'httpMethod': event['requestContext']['httpMethod'],
+#         'pathParameters': event['pathParameters'],
+#         'body': event['body']
+#     }
+
+#     response = invoke_order_lambda(payload, 'RequestResponse')
+#     return response
+
+
+# def create_order(event, status):
+#     """
+#     create new order
+#     """
+
+#     #convert unicode to dict
+#     event['body'] = json.loads(event['body'])
+
+#     #add order metadata
+#     event['body']['orderdate'] = timegm(datetime.now().timetuple())
+#     event['body']['paymentstatus'] = status
+
+#     #call order microservice with POST request
+#     response = order(event)
+#     data = json.loads(response['Payload'].read())
+
+#     return data
