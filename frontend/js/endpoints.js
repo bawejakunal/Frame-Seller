8e144fa54463fdedad190a9fbfc6ce94c31e995b
Akshay Nagpal <akshay2626@gmail.com>
2017-04-24 22:04:52 -0400

    added pending orders endpoint

--- frontend/js/endpoints.js
+++ frontend/js/endpoints.js
@@ -2,6 +2,7 @@ var baseURL = "https://82d0motn04.execute-api.us-east-1.amazonaws.com/prod";
 var authTokenEndpoint = baseURL+"/authorize/login/";
 var signUpEndpoint = baseURL+"/authorize/signup/";
 var orderEndpoint = baseURL+"/orders/";
+var pendingOrderEndpoint = baseURL + "/orderqueue/";
 var productEndPoint = baseURL+"/products/";
 var verifyCustomerEndPoint = baseURL+"/authorize/verify/";
 var purchaseEndPoint = baseURL+"/purchase/"
